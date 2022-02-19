2cff3097438785ac35e3284e25ed4a1d0b768a5e
xianyuzhizun <1738099872@qq.com>
2022-02-19 21:59:42 +0800

    <refactor> Activity基类加入开启广播判断参数

--- app/src/main/java/com/example/wanAndroid/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/wanAndroid/ui/base/BaseActivity.kt
@@ -12,8 +12,10 @@ import com.example.wanAndroid.ui.receiver.NetworkConnectChangedReceiver
  * Created by 咸鱼至尊 on 2021/12/9
  *
  * desc: Activity基类 目前只用来监听网络状态变化|状态栏沉浸|转场动画效果，可选择继承
+ *
+ * @property receive 是否接收广播(默认接收)
  */
-open class BaseActivity : AppCompatActivity() {
+open class BaseActivity(private val receive: Boolean = true) : AppCompatActivity() {
 
     //延迟初始化网络状态变化监听器
     private val mNetworkChangeListener by lazy { NetworkConnectChangedReceiver() }
@@ -31,13 +33,13 @@ open class BaseActivity : AppCompatActivity() {
         //添加网络状态变化意图
         filter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
         //在程序恢复时动态注册广播接收器，确保只有处于栈顶的activity可以接收广播
-        registerReceiver(mNetworkChangeListener, filter)
+        if (receive) registerReceiver(mNetworkChangeListener, filter)
     }
 
     override fun onPause() {
         super.onPause()
         //同理，在程序暂停时销毁动态注册的广播接收器(当程序处于后台时不会接收广播)
-        unregisterReceiver(mNetworkChangeListener)
+        if (receive) unregisterReceiver(mNetworkChangeListener)
     }
 
     @Suppress("DEPRECATION")
